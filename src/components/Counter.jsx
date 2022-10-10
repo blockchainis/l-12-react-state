3d406712d8ea29e06865cea35c9f954bf39c94f1
hoon2 <sanghoon.engineer@gmail.com>
2022-10-10 12:32:28 +0900

    12-state초기코드

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
 function Counter() {
-  const [number, setNumber] = useState(0);
-  function handlePlusClick() {
-    setNumber(number + 1);
-  }
-  function handleMinusClick() {
-    setNumber(number - 1);
-  }
-
+  let number = 0;
   return (
     <>
       <h1> {number} </h1>
-      <button onClick={handlePlusClick}> + </button>
-      <button onClick={handleMinusClick}> - </button>
+      <button
+        onClick={() => {
+          number += 1;
+        }}
+      ></button>
+      <button
+        onClick={() => {
+          number -= 1;
+        }}
+      >
+        -
+      </button>
     </>
   );
 }
